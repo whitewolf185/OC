57db3278c2c0f6d2b194dc73f783da533861cbaf
whitewolf185 <whitewolf.mot185@gmail.com>
2020-10-19 00:07:28 +0300

    ready ready

--- lab 2/child.c
+++ lab 2/child.c
@@ -1,4 +1,5 @@
 #include "unistd.h"
+#include "stdio.h"
 
 
 struct numbers {
@@ -9,11 +10,19 @@ int main() {
 //TODO здесь нужно пользоваться read, у которого первый аргумент будет ноль.
 // pipe распределяется сам как ему надо
     struct numbers nums;
+    FILE* output = NULL;
+    output = fopen("out.txt","a");
+    if(output == NULL){
+        printf("file error");
+        fclose(output);
+        return 1;
+    }
     while(read(STDIN_FILENO, &nums, sizeof(struct numbers)) > 0) {
 
         int result = nums.n1 + nums.n2 + nums.n3;
         write(STDOUT_FILENO, &result, sizeof(int));
+        fprintf(output, "%d + %d + %d = %d\n",nums.n1, nums.n2, nums.n3, result);
     }
-
+    fclose(output);
     return 0;
 }
