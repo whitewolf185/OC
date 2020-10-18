3e14f6b84f2d02fb988c61a7148811faec35daa3
Whitewolf185 <matvey2001xxl1976@mail.ru>
2020-10-18 20:03:56 +0300

    i have some problems

--- lab 2/child.c
+++ lab 2/child.c
@@ -1,10 +1,17 @@
 #include "unistd.h"
 #include "stdio.h"
 
+struct numbers {
+    int n1, n2, n3;
+};
+
 int main() {
 //TODO здесь нужно пользоваться read, у которого первый аргумент будет ноль.
 // pipe распределяется сам как ему надо
-    int result = n1 + n2 + n3;
+    struct numbers nums;
+    read(0, &nums, sizeof(struct numbers));
+
+    int result = nums.n1 + nums.n2 + nums.n3;
     write(STDOUT_FILENO, &result, sizeof(int));
 
     return 0;
--- lab 2/main.c
+++ lab 2/main.c
@@ -22,34 +22,52 @@ int main() {
         printf("Fork error!");
         return 1;
     }
+
+//   ----------- parent----------
     else if (ID > 0) {//parent
         printf("This is parent\n");
         fflush(stdout);
-        close(fd2[1]);//write
-        close(fd1[0]);//read
+
+
+        close(fd2[1]);
+        close(fd1[0]);
+        close(fd1[1]);
 
         int result;
         read(fd2[0], &result, sizeof(int));
         printf("result is %d\n", result);
         fflush(stdout);
         close(fd2[0]);
-
-//      TODO нужно сделать переопределение дескриптора с помощью dup2.
-//       И читать scanf нужно здесь
-        struct numbers nums;
-        scanf("%d %d %d", &nums.n1, &nums.n2, &nums.n3);
-        write(fd1[1], &nums, sizeof(struct numbers));
-        void *arg = NULL;
-        dup2()
-        execl("child.c", arg);
-        close(fd1[1]);
     }
 
+
+        //---------child----------
     else {//child
-        close(fd2[0]);
         printf("This is child\n");
         fflush(stdout);
 
+        close(fd2[1]);//write
+        close(fd2[0]);
+        close(fd1[0]);//read
+
+        struct numbers nums;
+
+        if (dup2(fd1[1], STDOUT_FILENO) < 0) {
+            printf("Dup error\n");
+            return 1;
+        }
+
+        printf("Enter nums\n");
+        fflush(stdout);
+        scanf("%d %d %d", &nums.n1, &nums.n2, &nums.n3);
+        write(fd1[1], &nums, sizeof(struct numbers));
+
+        char *const *arg = NULL;
+        if (execv("child.out", arg) < 0) {
+            printf("Exec error\n");
+            return 1;
+        }
+        close(fd1[1]);
 
     }
 
