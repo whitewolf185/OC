ad083fa97bb46a3ec819e18f9efc3a11fd7ccc1f
whitewolf185 <whitewolf.mot185@gmail.com>
2020-12-01 20:56:42 +0300

    i think it is gonna work. I have added window setting

--- lab3/main.c
+++ lab3/main.c
@@ -11,6 +11,7 @@ struct Matrix out;
 
 typedef struct tr_data {
     int x,y;
+    int winX, winY;
 
 }tr_data;
 
@@ -20,6 +21,20 @@ typedef struct tr_data {
 
 int k,n,m;
 
+int max(int lhs, int rhs){
+    if(lhs > rhs){
+        return lhs;
+    }
+    return rhs;
+}
+
+int min(int lhs, int rhs){
+    if(lhs < rhs){
+        return lhs;
+    }
+    return rhs;
+}
+
 void* threadFunc(void* Tdata){
     //printf("im alive\n");
     fflush(stdout);
@@ -27,213 +42,26 @@ void* threadFunc(void* Tdata){
     vector pixels;
     create(&pixels,0);
 
-#ifdef DEBUG
-    printf("Im in threadFunc with Id = %d %d\n", id->x, id->y);
-    fflush(stdout);
-#endif //DEBUG
-
-    //угловые пиксели
-    if(id->x == 0 && id->y == 0
-       || id->x == n-1 && id->y == m-1
-       || id->x == 0 && id->y == m-1
-       || id->x == n-1 && id->y == 0)
-    {
-
-#ifdef DEBUG
-        printf("Im in firs if with Id = %d %d; m = %d; n = %d\n", id->x, id->y,m,n);
-        fflush(stdout);
-#endif //DEBUG
-
-        int flagX = (id->x == 0)? 1 : -1;
-        int flagY = (id->y == 0)? 1 : -1;
-
-        if(m > 1 && n > 1){
-            push_back(&pixels,in.matr[id->x][id->y]);
-            push_back(&pixels,in.matr[id->x+flagX][id->y]);
-            push_back(&pixels,in.matr[id->x][id->y+flagY]);
-            push_back(&pixels,in.matr[id->x+flagX][id->y+flagY]);
-
-
-#ifdef DEBUG
-            printf("This is pixels before MERGE\n");
-            print_all(&pixels);
-            fflush(stdout);
-#endif //DEBUG
-
+    int cirI, curJ;
+    for (int i = max(0, id->x - id->winX); i < min(n, id->x + id->winX); ++i) {
+        for (int j = max(0, id->y - id->winY); j < min(m, id->y + id->winY); ++j) {
+            push_back(&pixels,in.matr[i][j]);
             merge(&pixels,0,pixels.size-1);
-
-#ifdef DEBUG
-            printf("This is pixels after MERGE\n");
-            print_all(&pixels);
-            fflush(stdout);
-#endif //DEBUG
-
-#ifdef DEBUG
-            printf("\nShow me Matrix OUT\n");
-            printMatrix(&out);
-            fflush(stdout);
-#endif //DEBUG
-
-            out.matr[id->x][id->y] = (pixels.data[1] + pixels.data[2]) / 2;
-
-#ifdef DEBUG
-            printf("\nShow me Matrix OUT again\n");
-            printMatrix(&out);
-            fflush(stdout);
-#endif //DEBUG
-
-            destroy(&pixels);
-            return NULL;
-        }
-        else{
-            out.matr[id->x][id->y] = in.matr[id->x][id->y];
-            return NULL;
         }
     }
 
-
-        //края без угловых элементов
-    else if(id->x == 0 ||  id->y == 0 ||
-            id->x == n-1 || id->y == m-1)
-    {
-
-#ifdef DEBUG
-        printf("Im in second if with Id = %d %d; m = %d; n = %d\n", id->x, id->y,m,n);
-        fflush(stdout);
-#endif //DEBUG
-
-        if(id->x == 0){
-            if(m > 1 && n > 1){
-                push_back(&pixels,in.matr[id->x][id->y]);
-
-                push_back(&pixels,in.matr[id->x+1][id->y]);
-                push_back(&pixels,in.matr[id->x][id->y+1]);
-                push_back(&pixels,in.matr[id->x][id->y-1]);
-
-                push_back(&pixels,in.matr[id->x+1][id->y+1]);
-                push_back(&pixels,in.matr[id->x+1][id->y-1]);
-
-
-
-                merge(&pixels,0,pixels.size-1);
-
-                out.matr[id->x][id->y] = pixels.data[pixels.size/2];
-                destroy(&pixels);
-                return NULL;
-            }
-            else{
-                out.matr[id->x][id->y] = in.matr[id->x][id->y];
-                return NULL;
-            }
-        }
-
-        if(id->y == 0){
-            if(m > 1 && n > 1){
-                push_back(&pixels,in.matr[id->x][id->y]);
-
-                push_back(&pixels,in.matr[id->x][id->y+1]);
-                push_back(&pixels,in.matr[id->x+1][id->y]);
-                push_back(&pixels,in.matr[id->x-1][id->y]);
-
-                push_back(&pixels,in.matr[id->x+1][id->y+1]);
-                push_back(&pixels,in.matr[id->x-1][id->y+1]);
-
-
-                merge(&pixels,0,pixels.size-1);
-
-                out.matr[id->x][id->y] = pixels.data[pixels.size/2];
-                destroy(&pixels);
-                return NULL;
-            }
-            else{
-                out.matr[id->x][id->y] = in.matr[id->x][id->y];
-                return NULL;
-            }
-        }
-
-
-
-        if(id->x == n-1){
-            if(m > 1 && n > 1){
-                push_back(&pixels,in.matr[id->x][id->y]);
-
-                push_back(&pixels,in.matr[id->x-1][id->y]);
-                push_back(&pixels,in.matr[id->x][id->y+1]);
-                push_back(&pixels,in.matr[id->x][id->y-1]);
-
-                push_back(&pixels,in.matr[id->x-1][id->y+1]);
-                push_back(&pixels,in.matr[id->x-1][id->y-1]);
-
-
-
-                merge(&pixels,0,pixels.size-1);
-
-                out.matr[id->x][id->y] = pixels.data[pixels.size/2];
-                destroy(&pixels);
-                return NULL;
-            }
-            else{
-                out.matr[id->x][id->y] = in.matr[id->x][id->y];
-                return NULL;
-            }
-        }
-
-        if(id->y == m-1){
-            if(m > 1 && n > 1){
-                push_back(&pixels,in.matr[id->x][id->y]);
-
-                push_back(&pixels,in.matr[id->x][id->y-1]);
-                push_back(&pixels,in.matr[id->x+1][id->y]);
-                push_back(&pixels,in.matr[id->x-1][id->y]);
-
-                push_back(&pixels,in.matr[id->x+1][id->y-1]);
-                push_back(&pixels,in.matr[id->x-1][id->y-1]);
-
-
-                merge(&pixels,0,pixels.size-1);
-
-                out.matr[id->x][id->y] = pixels.data[pixels.size/2];
-                destroy(&pixels);
-                return NULL;
-            }
-            else{
-                out.matr[id->x][id->y] = in.matr[id->x][id->y];
-                return NULL;
-            }
-        }
-
+    if(m>1 && n > 1){
+        out.matr[id->x][id->y] = pixels.data[pixels.size/2];
+        destroy(&pixels);
+        return NULL;
+    }
+    else {
+        out.matr[id->x][id->y] = in.matr[id->x][id->y];
+        return NULL;
     }
 
-
-        //все остальные случаи
-    else{
-        if(m > 1 && n > 1){
-            push_back(&pixels,in.matr[id->x][id->y]);
-
-            push_back(&pixels,in.matr[id->x+1][id->y]);
-            push_back(&pixels,in.matr[id->x][id->y+1]);
-            push_back(&pixels,in.matr[id->x+1][id->y+1]);
-
-            push_back(&pixels,in.matr[id->x-1][id->y]);
-            push_back(&pixels,in.matr[id->x][id->y-1]);
-            push_back(&pixels,in.matr[id->x-1][id->y-1]);
-
-            push_back(&pixels,in.matr[id->x-1][id->y+1]);
-
-            push_back(&pixels,in.matr[id->x+1][id->y-1]);
-
-            merge(&pixels,0,pixels.size-1);
-
-            out.matr[id->x][id->y] = pixels.data[pixels.size/2];
-            destroy(&pixels);
-            return NULL;
-        }
-        else{
-            out.matr[id->x][id->y] = in.matr[id->x][id->y];
-            return NULL;
-        }
-    }
 }
+
 
 
 int main(int argc, char const *argv[]){
@@ -242,24 +70,7 @@ int main(int argc, char const *argv[]){
         max_threads = atoi(argv[1]);
     }
 
-#ifdef MERGE
-    struct vector mer;
-    create(&mer,0);
-    srand(time(NULL));
-    for (int i = 0; i < 4; ++i) {
-        int a;
-        scanf("%d",&a);
-        push_back(&mer,a);
-    }
-
-    printf("-|");
-    print_all(&mer);
-    merge(&mer,0,mer.size-1);
-    printf("-|");
-    print_all(&mer);
-// 8 9 7 8 5 1 5 9 5 6
-    return 0;
-#endif
+    int winX, winY;
 
     printf("Threads %d\nEnter size of matrix. Enter N and M\n> ",max_threads);
     fflush(stdout);
@@ -273,16 +84,22 @@ int main(int argc, char const *argv[]){
     scanf_With_Fill_Rand_Matrix(&in, n, m);
     //printMatrix(&in);
 
+    printf("Enter your window\n> ");
+    fflush(stdout);
+    scanf("%d %d", &winX, &winY);
+
     printf("Enter your K\n> ");
     fflush(stdout);
     scanf("%d", &k);
 
     pthread_t* threads = (pthread_t*) malloc(sizeof(pthread_t)*max_threads);
 
-    time_t start,end;
+    double start,end;
 
     struct tr_data* args = (struct tr_data*) malloc(sizeof(struct tr_data)*n*m);
-    start = time(NULL);
+    start = clock();
+    args->winX = winX;
+    args->winY = winY;
     for (int h = 0; h < k; ++h) {
         scanf_Matrix_with_Fill_0(&out,n,m);
 #ifdef DEBUG
@@ -328,7 +145,7 @@ int main(int argc, char const *argv[]){
 #endif //DEBUG
         swap(&in, &out);
     }
-    end = time(NULL);
+    end = clock();
 
     free(threads);
     free(args);
@@ -337,7 +154,7 @@ int main(int argc, char const *argv[]){
     //printMatrix(&in);
 
     FILE *file = fopen("log.txt", "a");
-    fprintf(file, "threads = %d\ntime = %ld s\n\n", max_threads,end-start);
+    fprintf(file, "threads = %d\ntime = %f s\n\n", max_threads,(end-start)/1000);
     fclose(file);
 
     return 0;
