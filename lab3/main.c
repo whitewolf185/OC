8d0f99efdfe799810b96ac66a7969f6447fa8227
whitewolf185 <whitewolf.mot185@gmail.com>
2020-12-03 16:05:18 +0300

    ready

--- lab3/main.c
+++ lab3/main.c
@@ -17,7 +17,7 @@ typedef struct tr_data {
 
 #define RAND_MAX 10
 //#define DEBUG
-//#define MERGE
+//#define ENTER
 
 int k,n,m;
 
@@ -41,22 +41,45 @@ void* threadFunc(void* Tdata){
     struct tr_data* id = (struct tr_data*) Tdata;
     vector pixels;
     create(&pixels,0);
-
-    int cirI, curJ;
-    for (int i = max(0, id->x - id->winX); i < min(n, id->x + id->winX); ++i) {
-        for (int j = max(0, id->y - id->winY); j < min(m, id->y + id->winY); ++j) {
+#ifdef DEBUG
+    printf("Border: %d %d\n",( (id->winX - 1)/2 ), ( (id->winY - 1) / 2 ));
+#endif //DEBUG
+    for (int i = max(0, id->x - ((id->winX - 1)/2)); i <= min(n-1, id->x + ((id->winX - 1) / 2)); ++i) {
+        for (int j = max(0, id->y - ((id->winY - 1) / 2)); j <= min(m-1, id->y + ((id->winY - 1) / 2)); ++j) {
+#ifdef DEBUG
+            printf("IN FOR %di %dj\nmatrix %d\n",i,j,in.matr[i][j]);
+            fflush(stdout);
+#endif //DEBUG
             push_back(&pixels,in.matr[i][j]);
-            merge(&pixels,0,pixels.size-1);
         }
     }
 
-    if(m>1 && n > 1){
+#ifdef DEBUG
+    printf("_____\n");
+    printf("BEFORE MERGE  id is %dx %dy\n",id->x,id->y);
+    print_all(&pixels);
+    printf("\n");
+    fflush(stdout);
+#endif //DEBUG
+
+    merge(&pixels,0,pixels.size-1);
+
+#ifdef DEBUG
+    printf("AFTER MERGE  id is %dx %dy\n",id->x,id->y);
+    print_all(&pixels);
+    printf("\n");
+    fflush(stdout);
+#endif //DEBUG
+
+
+    if(m > 1 && n > 1){
         out.matr[id->x][id->y] = pixels.data[pixels.size/2];
         destroy(&pixels);
         return NULL;
     }
     else {
         out.matr[id->x][id->y] = in.matr[id->x][id->y];
+        destroy(&pixels);
         return NULL;
     }
 
@@ -82,11 +105,22 @@ int main(int argc, char const *argv[]){
     }
 
     scanf_With_Fill_Rand_Matrix(&in, n, m);
-    //printMatrix(&in);
-
+#ifdef ENTER
+    printMatrix(&in);
+#endif
     printf("Enter your window\n> ");
     fflush(stdout);
     scanf("%d %d", &winX, &winY);
+
+    //условия задания окна
+    if( winX != winY ||
+        winX == 0 || winY == 0 ||
+        winX > n || winY > m ||
+        winX % 2 == 0)
+    {
+        printf("I cannot work with this window\n");
+        return 0;
+    }
 
     printf("Enter your K\n> ");
     fflush(stdout);
@@ -98,12 +132,11 @@ int main(int argc, char const *argv[]){
 
     struct tr_data* args = (struct tr_data*) malloc(sizeof(struct tr_data)*n*m);
     start = clock();
-    args->winX = winX;
-    args->winY = winY;
+
     for (int h = 0; h < k; ++h) {
         scanf_Matrix_with_Fill_0(&out,n,m);
 #ifdef DEBUG
-        printf("Im here with K=%d\n",k);
+        printf("Im here with K=%d\n\n",k);
         fflush(stdout);
 #endif //DEBUG
         int count = 0;
@@ -111,6 +144,8 @@ int main(int argc, char const *argv[]){
             for (int j = 0; j < m; ++j) {
                 args[count].x = i;
                 args[count].y = j;
+                args[count].winX = winX;
+                args[count].winY = winY;
                 ++count;
             }
         }
@@ -151,11 +186,21 @@ int main(int argc, char const *argv[]){
     free(args);
 
     printf("Result matrix:\n");
-    //printMatrix(&in);
+#ifdef ENTER
+    printMatrix(&in);
+#endif
 
     FILE *file = fopen("log.txt", "a");
-    fprintf(file, "threads = %d\ntime = %f s\n\n", max_threads,(end-start)/1000);
+    FILE* thread = fopen("D:\\Documents\\Projects\\c++\\OC\\venv\\treads.txt","a");
+    FILE* time = fopen("D:\\Documents\\Projects\\c++\\OC\\venv\\time.txt","a");
+
+    fprintf(file, "threads = %d\ntime = %fms\n\n", max_threads,(end-start)/1000);
+    fprintf(thread, "%d\n",max_threads);
+    fprintf(time,"%f\n",(end-start)/1000);
+
     fclose(file);
+    fclose(thread);
+    fclose(time);
 
     return 0;
 }
