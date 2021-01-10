a2dc6f3fc11fe98134c4a2cdab36aadbaa60a507
whitewolf185 <whitewolf.mot185@gmail.com>
2021-01-10 14:26:40 +0300

    initial commit lab 6-8

---
+++ lab6-8/client.cpp
@@ -0,0 +1,262 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <zmqpp/zmqpp.hpp>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <signal.h>
+#include <map>
+
+#include "network.h"
+#define DEBUG
+
+int id = -1, next_id = -1, prev_id = -1;
+std::unique_ptr<zmqpp::socket> front_in(nullptr), front_out(nullptr), front_ping(nullptr),
+        back_in(nullptr), back_out(nullptr), back_ping(nullptr);
+std::string back_in_port, back_out_port, back_ping_port, front_ping_port;
+zmqpp::context context;
+
+
+void* ping(void*) {
+    int packet[2];
+    size_t length = 2 * sizeof(int);
+    while (*front_ping) {
+        front_ping->receive_raw(reinterpret_cast<char *>(packet), length);
+        // std::cout << "received ping " << packet[0] << " " << packet[1] << std::endl;
+        if (packet[0] != id) {
+            // std::cout << "sending forward" << std::endl;
+            if (back_ping.get() != nullptr) {
+                if (!back_ping->send_raw(reinterpret_cast<char *>(packet), length, zmqpp::socket::dont_wait)) {
+                    packet[1] = 0;
+                } else if (!back_ping->receive_raw(reinterpret_cast<char *>(packet), length)) {
+                    packet[1] = 0;
+                    back_ping->close();
+                    back_ping.reset(new zmqpp::socket(context, zmqpp::socket_type::req));
+                    back_ping->set(zmqpp::socket_option::receive_timeout, 1000);
+                    back_ping->connect(host + back_ping_port);
+                }
+            } else {
+                front_ping->close();
+                front_ping.reset(new zmqpp::socket(context, zmqpp::socket_type::rep));
+                front_ping->bind(host + front_ping_port);
+                continue;
+            }
+        }
+        // std::cout << "sending back" << std::endl;
+        front_ping->send_raw(reinterpret_cast<char *>(packet), length);
+    }
+    return NULL;
+}
+
+
+#ifdef DEBUG
+[[noreturn]] void* awake(void* arg){
+    bool* flag = static_cast<bool*>(arg);
+    while(true) {
+        sleep(5);
+        std::cout << "im making flag true" << std::endl;
+        *flag = true;
+    }
+}
+#endif
+
+
+void* back_to_front(void*) {
+    zmqpp::message msg;
+    //std::cout << "im in back_to_front" << std::endl; //debug
+#ifdef DEBUG
+    bool flag = true;
+    pthread_t flag_awake;//debug
+    check(pthread_create(&flag_awake, NULL, awake, (void*) &flag),
+          -1,
+          "pthread_create error");
+    if(pthread_detach(flag_awake) != 0){
+        perror("detach error");
+    }
+#endif
+
+    while (back_in.get() == nullptr || *back_in) {
+#ifdef DEBUG
+        if(flag){
+            std::cout << "im in back_to_front" << std::endl; //debug
+            std::cout << back_in_port << std::endl;
+        }
+#endif
+
+        if (back_in.get() == nullptr) {
+            flag = false;
+            continue;
+        }
+
+        back_in->receive(msg);
+        // std::cout << "sending back" << std::endl;
+        int rid;
+        msg >> rid;
+        if (rid != id) {
+            front_out->send(msg);
+            continue;
+        }
+
+        int act;
+        msg >> act;
+        switch (static_cast<action>(act)) {
+            case action::rebind_back: {
+                msg >> next_id;
+                back_out->disconnect(host + back_out_port);
+                msg >> back_out_port;
+                back_out->connect(host + back_out_port);
+
+                back_in->unbind(host + back_in_port);
+                msg >> back_in_port;
+                back_in->bind(host + back_in_port);
+
+                back_ping->disconnect(host + back_ping_port);
+                msg >> back_ping_port;
+                back_ping->connect(host + back_ping_port);
+                break;
+            }
+
+            case action::unbind_back: {
+                back_in.reset(nullptr);
+                back_out.reset(nullptr);
+                back_ping.reset(nullptr);
+                next_id = -1;
+                break;
+            }
+
+            default: {}
+        }
+    }
+    return NULL;
+}
+
+
+
+int main(int argc, char* argv[]){
+    assert(argc >= 4);
+    id = strtoul(argv[1], nullptr, 10);
+    prev_id = strtoul(argv[2], nullptr, 10);
+    std::string bridge_port(argv[3]);
+    // std::cout << getpid() << " " << id << std::endl;
+
+    zmqpp::message msg;
+
+    zmqpp::socket bridge(context, zmqpp::socket_type::req);
+    bridge.connect(host + bridge_port);
+
+    front_in.reset(new zmqpp::socket(context, zmqpp::socket_type::pull));
+    std::string front_in_port = std::to_string(try_bind(*front_in));
+
+    front_ping.reset(new zmqpp::socket(context, zmqpp::socket_type::rep));
+    front_ping_port = std::to_string(try_bind(*front_ping));
+
+    msg << front_in_port << front_ping_port;
+    bridge.send(msg);
+
+    bridge.receive(msg);
+
+    std::string front_out_port;
+    msg >> front_out_port;
+    front_out.reset(new zmqpp::socket(context, zmqpp::socket_type::push));
+    front_out->connect(host + front_out_port);
+
+    bridge.disconnect(host + bridge_port);
+    bridge.close();
+    bridge = zmqpp::socket(context, zmqpp::socket_type::rep);
+    bridge_port = std::to_string(try_bind(bridge));
+
+    {
+        zmqpp::message ans;
+        ans << id << static_cast<int>(action::fork) << getpid();
+        front_out->send(ans);
+#ifdef DEBUG
+        std::cout << "sending the msg"<< std::endl;
+#endif
+    }
+
+    pthread_t ping_id;
+    check(pthread_create(&ping_id, NULL, ping, NULL), -1, "pthread_create error");
+    if (pthread_detach(ping_id) != 0) {
+        perror("detach error");
+    }
+
+    pthread_t back_to_front_id;
+    check(pthread_create(&back_to_front_id, NULL, back_to_front, NULL), -1, "pthread_create error");
+    if (pthread_detach(back_to_front_id) != 0) {
+        perror("detach error");
+    }
+
+    int act;
+    int tid;
+
+    while (true) {
+        if (!front_in->receive(msg)) {
+            perror("");
+        }
+
+        msg >> tid;
+
+        if (tid != id) {
+            // std::cout << "sending forward" << std::endl;
+            back_out->send(msg);
+            continue;
+        }
+
+        msg >> act;
+
+        switch (static_cast<action>(act)) {
+            case action::fork: {
+                int cid;
+                msg >> cid;
+
+
+                if (next_id == -1) {
+                    next_id = cid;
+
+                    int pid = fork();
+                    check(pid, -1, "fork error");
+                    if (pid == 0) {
+                        check(execl("client", "client", std::to_string(cid).c_str(), std::to_string(id).c_str(),
+                                    bridge_port.c_str(), NULL), -1, "execl error");
+                    }
+
+
+                    back_in.reset(new zmqpp::socket(context, zmqpp::socket_type::pull));
+                    back_in_port = std::to_string(try_bind(*back_in));
+
+                    zmqpp::message ports;
+                    bridge.receive(ports);
+
+                    ports >> back_out_port >> back_ping_port;
+                    ports.pop_back();
+                    ports.pop_back();
+
+                    back_out.reset(new zmqpp::socket(context, zmqpp::socket_type::push));
+                    back_ping.reset(new zmqpp::socket(context, zmqpp::socket_type::req));
+                    back_ping->set(zmqpp::socket_option::receive_timeout, 1000);
+
+                    back_out->connect(host + back_out_port);
+                    back_ping->connect(host + back_ping_port);
+
+                    ports << back_in_port;
+                    bridge.send(ports);
+                }
+                std::cout << "im in fork" << std::endl;
+                break;
+            }
+
+            case action::start: {
+                pthread_t timer;
+                check(pthread_create(&timer, NULL, timer_control, (void*) action::start),
+                      -1,
+                      "pthread_create error");
+            }
+
+            default:{}
+        }
+    }
+
+
+    return 0;
+}
---
+++ lab6-8/server_fin.cpp
@@ -0,0 +1,288 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <zmqpp/zmqpp.hpp>
+#include <iostream>
+#include <list>
+#include <string>
+#include <signal.h>
+#include <sstream>
+
+
+#include "network.h"
+
+struct nodes{
+    int id;
+    std::list<nodes> bros;
+};
+
+using line_t = std::pair <nodes, std::pair<//changed
+        std::pair < zmqpp::socket, std::string>, std::pair<zmqpp::socket, std::string>>>;
+std::list <line_t> network;
+
+class node_coord {
+private:
+    std::pair <std::list<line_t>::iterator, nodes> data;
+
+public:
+    node_coord(std::list<line_t>::iterator &i1,nodes &i2) : data(i1, i2) {}
+
+    node_coord(std::list<line_t>::iterator &&i1,nodes &&i2) : data(i1, i2) {}
+
+    node_coord(node_coord &other) : data(other.data) {}
+
+    node_coord(node_coord &&other) : data(other.data) {}
+
+    std::list<line_t>::iterator &line() {
+        return data.first;
+    }
+
+    nodes &node() {
+        return data.second;
+    }
+
+    int &id() {
+        return data.second.id;
+    }
+
+    int &parent() {
+        // main parent
+        return data.first->first.id;
+    }
+
+    zmqpp::socket &out_sock() {
+        return data.first->second.first.first;
+    }
+
+    zmqpp::socket &ping_sock() {
+        return data.first->second.second.first;
+    }
+
+    std::string &out_port() {
+        return data.first->second.first.second;
+    }
+
+    std::string &ping_port() {
+        return data.first->second.second.second;
+    }
+};
+
+zmqpp::context context;
+
+std::list<nodes>::iterator find_node(nodes& b_it, int id){
+    auto result = b_it.bros.end();
+    for(auto iter = b_it.bros.begin(); iter != b_it.bros.end() && b_it.bros.size() != 0; ++iter){
+        if(iter->id == id){
+            return iter;
+        }
+
+        result = find_node(*iter,id);
+        if(result != iter->bros.end() && result->id == id){
+            return result;
+        }
+
+        result = b_it.bros.end();
+    }
+    return result;
+}
+
+node_coord find_node(int id){
+    for(auto b_it = network.begin(); b_it != network.end(); ++b_it){
+        if(b_it->first.id == id){
+            return {b_it, b_it->first};
+        }
+
+        auto result = find_node(b_it->first,id);
+        if(result != b_it->first.bros.end() && result->id == id){
+            return {b_it,*result};
+        }
+    }
+    return {network.end(), nodes()};
+}
+
+void* result_waiter(void* arg){
+    zmqpp::socket* in_sock = reinterpret_cast<zmqpp::socket*> (arg);
+    zmqpp::message msg;
+    int act, rid;
+
+    while(*in_sock){
+        in_sock->receive(msg);
+        msg >> rid >> act;
+
+        switch(static_cast<action>(act)){
+            case action::test: {
+                std::cout << "Hello " << rid << std::endl;
+                break;
+            }
+
+            case action::fork: {
+                int pid;
+                msg >> pid;
+                std::cout << "OK: " << pid << std::endl;
+                break;
+            }
+
+            case action::exit: {
+                node_coord node = find_node(rid);
+            }
+
+            default: {}
+        }
+    }
+    return NULL;
+}
+
+bool ping(zmqpp::socket &ping_sock, std::string &ping_port, int id) {
+    int packet[2] = {id, 1};
+    size_t length = 2 * sizeof(int);
+    if (!ping_sock.send_raw(reinterpret_cast<char *>(packet), length, zmqpp::socket::dont_wait)) {
+        return false;
+    }
+    if (!ping_sock.receive_raw(reinterpret_cast<char *>(packet), length)) {
+        ping_sock.close();
+        ping_sock = zmqpp::socket(context, zmqpp::socket_type::req);
+        ping_sock.set(zmqpp::socket_option::receive_timeout, 1000);
+        ping_sock.connect(host + ping_port);
+        return false;
+    }
+    return (packet[1] == 1) ? true : false;
+}
+
+int main(){
+    zmqpp::socket in_sock(context, zmqpp::socket_type::pull);
+    std::string in_port = std::to_string(try_bind(in_sock));
+
+    zmqpp::socket bridge(context,zmqpp::socket_type::rep);
+    std::string bridge_port = std::to_string(try_bind(bridge));
+
+
+    //проверка создание потока
+    pthread_t result_waiter_id;
+    check(pthread_create(&result_waiter_id, NULL,result_waiter, (void*) &in_sock),
+          -1,
+          "pthread_create failed");
+
+    if(pthread_detach(result_waiter_id) != 0){
+        perror("detach error");
+    }
+
+    std::string command;
+    while(std::cout << "> " && std::cin >> command && command != "exit"){
+        if(command == "create"){
+            int id, parent;
+            std::cin >> id >> parent;
+
+            if (id == -1 || find_node(id).line() != network.end()) {
+                std::cerr << "Error: Already exists" << std::endl;
+                continue;
+            }
+
+            if (parent == -1) {
+                //создание узла родителя
+                network.push_back({nodes(),
+                                   std::make_pair(std::make_pair(zmqpp::socket(context, zmqpp::socket_type::push), ""),
+                                                  std::make_pair(zmqpp::socket(context, zmqpp::socket_type::req),
+                                                                 ""))});
+                network.back().first.id = id;
+                node_coord node(--network.end(), network.back().first);
+
+                int pid = fork();
+                check(pid, -1, "fork error");
+                if (pid == 0) {
+                    check(execl("client", "client", std::to_string(id).c_str(), std::to_string(-1).c_str(),
+                                bridge_port.c_str(), NULL), -1, "execl error")
+                }
+
+                zmqpp::message ports;
+                bridge.receive(ports);
+
+                ports >> node.out_port();
+                node.out_sock().connect(host + node.out_port());
+
+                ports >> node.ping_port();
+                node.ping_sock().connect(host + node.ping_port());
+                node.ping_sock().set(zmqpp::socket_option::receive_timeout, 1000);
+
+                ports.pop_back();
+                ports.pop_back();
+                ports << in_port;
+                bridge.send(ports);
+            }
+
+            else {
+                node_coord parent_node = find_node(parent);
+                if (parent_node.line() == network.end()) {
+                    std::cerr << "Error: Parent not found" << std::endl;
+                    continue;
+                }
+
+                if (!ping(parent_node.ping_sock(), parent_node.ping_port(), parent)) {
+                    std::cerr << "Error: Parent is unavailable" << std::endl;
+                    continue;
+                }
+
+                parent_node.line()->first.bros.push_back({id,std::list<nodes>()});
+
+                zmqpp::message msg;
+                msg << parent << static_cast<int>(action::fork) << id;
+                parent_node.out_sock().send(msg);
+            }
+        }
+
+        if(command == "exec"){
+            int id;
+            std::cin >> id;
+
+            node_coord node = find_node(id);
+
+            if (node.line() == network.end()) {
+                std::cerr << "Error: Not found" << std::endl;
+                continue;
+            }
+
+            if (!ping(node.ping_sock(), node.ping_port(), id)) {
+                std::cerr << "Error: Node is unavailable" << std::endl;
+                continue;
+            }
+
+            std::string act;
+            std::cin >> act;
+            zmqpp::message msg;
+            msg << id;
+            if(act == "start"){
+                msg << static_cast<int>(action::start);
+            }
+            else if(act == "stop"){
+                msg << static_cast<int>(action::stop);
+            }
+            else if(act == "time"){
+                msg << static_cast<int>(action::time);
+            }
+        }
+
+        if(command == "test"){
+            int pid = fork();
+            if(pid == 0){
+                execl("client", "client", bridge_port.c_str(), NULL);
+            }
+            zmqpp::message msg;
+            bridge.receive(msg);
+            int id;
+            msg >> id;
+            std::cout << id << std::endl;
+
+            msg.pop_back();
+            msg.pop_back();
+
+            msg << "im done";
+
+            bridge.send(msg);
+        }
+    }
+
+    return 0;
+}
+
+
+// topology - 2
+// commands - 3
+// ping     - 2 - ping id
